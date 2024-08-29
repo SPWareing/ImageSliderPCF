1b45beb299933bb59baaed2f58508853ca800c22
SPWareing <sw1662@my.bristol.ac.uk>
2024-08-29 07:17:23 +0100

    CSS changes

--- ImageSliderPCF/components/imageslider.tsx
+++ ImageSliderPCF/components/imageslider.tsx
@@ -6,8 +6,8 @@ import '../css/image-slider.css'
 
 export interface ImageSliderItem {
     id: number | string;
-    imageUrl: string ;
-    name?: string ;
+    imageUrl: string;
+    name?: string;
 }
 
 export interface ImageSliderProps {
@@ -18,45 +18,45 @@ export interface ImageSliderProps {
 }
 // defines the default image list if no images are passed in
 const IMAGES = [
-    {id : 1, name: "Placeholder 350x150", imageUrl:"https://fabricweb.azureedge.net/fabric-website/placeholders/350x150.png"},
-    {id: 2, name: "Placeholder 500x250",imageUrl:"https://fabricweb.azureedge.net/fabric-website/placeholders/500x250.png"}, 
-     ] as ImageSliderItem[];
+    { id: 1, name: "Placeholder 350x150", imageUrl: "https://fabricweb.azureedge.net/fabric-website/placeholders/350x150.png" },
+    { id: 2, name: "Placeholder 500x250", imageUrl: "https://fabricweb.azureedge.net/fabric-website/placeholders/500x250.png" },
+] as ImageSliderItem[];
 
 export const ImageSlider = React.memo((props: ImageSliderProps) => {
 
-    const { imageUrls = [] , width, iconColour } = props;
+    const { imageUrls = [], width, iconColour } = props;
     const [imageIndex, setImageIndex] = React.useState(0);
     const [imageList, setImageList] = React.useState<ImageSliderItem[]>(IMAGES);
 
     //To avoid the control crashing have a placeholder image if no images are passed in
-    const src = imageUrls?.length === 0? IMAGES: imageUrls;
-    
-                
+    const src = imageUrls?.length === 0 ? IMAGES : imageUrls;
+
+
     React.useEffect(() => {
-        
+
         setImageList(src);
-        //console.log("imageUrls changed:", imageUrls);
+       
         // Perform side effect here
-      }, [imageUrls]);
+    }, [imageUrls]);
 
     const classes = mergeStyleSets({
-        iconArrow:{
-        fontSize: "2em",
-        color:iconColour
-      },
-      iconDot:{
-        
-        color:iconColour
-      }
-    
+        iconArrow: {
+            fontSize: "2em",
+            color: iconColour
+        },
+        iconDot: {
+
+            color: iconColour
+        }
+
     });
 
-    
-    
 
 
-    const IconLeft = () => <Icon iconName="ChevronLeft" className={classes.iconArrow}/>;
-    const IconRight = () => <Icon iconName="ChevronRight" className={classes.iconArrow}/>;
+
+
+    const IconLeft = () => <Icon iconName="ChevronLeft" className={classes.iconArrow} />;
+    const IconRight = () => <Icon iconName="ChevronRight" className={classes.iconArrow} />;
     const IconCircle = () => <Icon iconName="LocationCircle" className={classes.iconDot} />;
     const IconCircleDot = () => <Icon iconName="Location" className={classes.iconDot} />;
 
@@ -82,9 +82,9 @@ export const ImageSlider = React.memo((props: ImageSliderProps) => {
 
     const rootStyle = React.useMemo(() => {
         return {
-            maxWidth:"1200px", 
+            maxWidth: "1200px",
             width: width,
-            aspectRatio:"16/9",
+            aspectRatio: "16/9",
             margin: "0 auto"
         } as React.CSSProperties;
     }, [width]);
@@ -94,59 +94,60 @@ export const ImageSlider = React.memo((props: ImageSliderProps) => {
 
     return (<div style={rootStyle}>
         <div style={{
-            width:"100%",
-            height:"100%",
-            position:"relative",
-            color:"rgba(21,21,21,0.9)",
+            width: "100%",
+            height: "100%",
+            position: "relative",
+            color: "rgba(21,21,21,0.9)",
             fontFamily: "Segoe UI,SegoeUI,'Helvetica Neue',Helvetica,Arial,sans-serif",
-            }}>
+        }}>
 
             <div style={{
-                width:"100%",
-                height:"100%",
-                display:"flex",
-                overflow:"hidden",
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                overflow: "hidden",
                 boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)"
             }} >
                 {imageList.map(url => {
-                   return <img 
-                   key={url.id} 
-                   src={url.imageUrl} 
-                   aria-label= {url.name? url.name : ""}
-                   className='img-slider-img'
-                   style={{ translate: `${-100 * imageIndex}%` }} onClick={downloadImage}
-                   loading = "lazy"
-                   />
+                    return <img
+                        key={url.id}
+                        src={url.imageUrl}
+                        aria-label={url.name ? url.name : ""}
+                        className='img-slider-img'
+                        style={{ translate: `${-100 * imageIndex}%` }} onClick={downloadImage}
+                        loading="lazy"
+                    />
                 })}
-            
-                </div>    
-            
-             
-             {imageIndex > 0 &&<button onClick={previousImage} className='img-slider-btn' style={{left:"0"}}>
-                <IconLeft /></button>}
-            {imageIndex < imageList.length -1 && <button onClick={nextImage} className='img-slider-btn' style={{right:"0"}}>
-                <IconRight/></button>}
-            
-            <div    style={{
-                position:"absolute",
-                bottom:"0.25rem",
-                width:"100%",
-                display:"flex",
-                justifyContent:"center",
-                alignItems:"center",
-                gap:"0.25rem"
-            
-            }}>
-                {imageList.map((_, index) => 
-                {return <button key={index} className='img-slider-dot-btn'
-                onClick={() => setImageIndex(index)}> { imageIndex === index? <IconCircleDot/> : <IconCircle />}</button>})}
+
             </div>
-                   {imageList[imageIndex].name }
-               
+            {imageList[imageIndex].name}
+
+            {imageIndex > 0 && <button onClick={previousImage} className='img-slider-btn' style={{ left: "0" }}>
+                <IconLeft /></button>}
+            {imageIndex < imageList.length - 1 && <button onClick={nextImage} className='img-slider-btn' style={{ right: "0" }}>
+                <IconRight /></button>}
+
+            <div style={{
+                position: "absolute",
+                bottom: "0.25rem",
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "0.25rem"
+
+            }}>
+                {imageList.map((_, index) => {
+                    return <button key={index} className='img-slider-dot-btn'
+                        onClick={() => setImageIndex(index)}> {imageIndex === index ? <IconCircleDot /> : <IconCircle />}</button>
+                })}
+            </div>
+
+
 
         </div>
-        
-        </div>
+
+    </div>
     );
 });
 
